425397be9bedd772bf51e4d4469ab0637f793020
Afaf Nasr <afafnaser226@gmail.com>
2026-02-07 21:02:38 +0200

    "CHORE: move command_exit to src folder and cleanup root directory"

--- command_exit.ts
+++
@@ -1,8 +0,0 @@
-import { type State } from "./state.js";
-
-export async function commandExit(state: State) {
-  console.log("Closing the Pokedex... Goodbye!");
-  state.rl.close(); 
-  process.exit(0);
-}
-
--- src/command_exit.ts
+++ src/command_exit.ts
@@ -2,6 +2,6 @@ import { type State } from "./state.js";
 
 export async function commandExit(state: State) {
   console.log("Closing the Pokedex... Goodbye!");
-  state.rl.close(); // إغلاق الواجهة بشكل نظيف
+  state.rl.close();
   process.exit(0);
 }
