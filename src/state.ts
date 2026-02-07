7933891fca49deee107eb04a3f686ca76a16f90f
Afaf Nasr <afafnaser226@gmail.com>
2026-02-07 15:16:01 +0200

    REFACTOR: introduce State object to manage shared program state

---
+++ src/state.ts
@@ -0,0 +1,41 @@
+import { type Interface } from "node:readline";
+
+
+export type CLICommand = {
+  name: string;
+  description: string;
+  callback: (state: State) => void; 
+};
+
+
+export type State = {
+  rl: Interface;
+  commands: Record<string, CLICommand>;
+};
+
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
+import { createInterface } from "node:readline";
+
+export function initState(): State {
+  const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "Pokedex > ",
+  });
+
+  const commands: Record<string, CLICommand> = {
+    exit: {
+      name: "exit",
+      description: "Exit the Pokedex",
+      callback: commandExit,
+    },
+    help: {
+      name: "help",
+      description: "Displays a help message",
+      callback: commandHelp,
+    },
+  };
+
+  return { rl, commands };
+}
