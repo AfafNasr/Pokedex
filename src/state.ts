cab4f488ff8de4e2bd0a6420668f04caac8bf55a
Afaf Nasr <afafnaser226@gmail.com>
2026-02-07 15:54:15 +0200

    CHORE: update State to handle pagination URLs and PokeAPI instance

--- src/state.ts
+++ src/state.ts
@@ -1,21 +1,26 @@
-import { type Interface } from "node:readline";
+import { createInterface, type Interface } from "node:readline";
+import { PokeAPI } from "./pokeapi.js";
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
+import { commandMap } from "./command_map.js";
+import { commandMapb } from "./command_mapb.js";
 
 
 export type CLICommand = {
   name: string;
   description: string;
-  callback: (state: State) => void; 
+  callback: (state: State) => Promise<void>; 
 };
 
 
 export type State = {
-  rl: Interface;
-  commands: Record<string, CLICommand>;
+  rl: Interface;                         
+  commands: Record<string, CLICommand>;  
+  pokeapi: PokeAPI;                      
+  nextLocationsURL: string | null;       
+  prevLocationsURL: string | null;       
 };
 
-import { commandExit } from "./command_exit.js";
-import { commandHelp } from "./command_help.js";
-import { createInterface } from "node:readline";
 
 export function initState(): State {
   const rl = createInterface({
@@ -24,18 +29,37 @@ export function initState(): State {
     prompt: "Pokedex > ",
   });
 
+  const pokeapi = new PokeAPI();
+
+  
   const commands: Record<string, CLICommand> = {
-    exit: {
-      name: "exit",
-      description: "Exit the Pokedex",
-      callback: commandExit,
-    },
     help: {
       name: "help",
       description: "Displays a help message",
       callback: commandHelp,
     },
+    exit: {
+      name: "exit",
+      description: "Exit the Pokedex",
+      callback: commandExit,
+    },
+    map: {
+      name: "map",
+      description: "Display the next 20 location areas",
+      callback: commandMap,
+    },
+    mapb: {
+      name: "mapb",
+      description: "Display the previous 20 location areas",
+      callback: commandMapb,
+    },
   };
 
-  return { rl, commands };
+  return {
+    rl,
+    commands,
+    pokeapi,
+    nextLocationsURL: null, 
+    prevLocationsURL: null,
+  };
 }
